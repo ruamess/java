c7dad2e9a04a43bfdb24f675ba7173e2946b4055
ruamess <zlaptiev@gmail.com>
2026-01-26 11:22:24 +0500

    add lab1

---
+++ lab1/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+
+  private static String fullName = "Иванов Иван Иванович";
+  private static String group = "ИС 25-25";
+  private static String speciality = "Информационные системы";
+
+  public static void main(String[] args) {
+    System.out.printf("%s\n%s\n%s\n",
+        fullName, group, speciality);
+  }
+}
